75900d17dff4a2c7d9508cf7d90836e6b61d3975
Rahulsharma2111 <rstv3644@gmail.com>
2025-03-16 12:06:15 +0530

    username, password and mobile number generate jwt token

---
+++ Token/src/main/java/JWT/Token/CustomTokenGenerate/User.java
@@ -0,0 +1,63 @@
+package JWT.Token.CustomTokenGenerate;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "usersTest")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+    private String mobileNumber;
+    private String token;
+
+    @Override
+    public Set<GrantedAuthority> getAuthorities() {
+        return Set.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
